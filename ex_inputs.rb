22c434fb72f28079b02a241f9ff307ce32eb769a
cmsrrent <rob.renton@gmail.com>
2018-10-28 19:37:05 +0000

    Ruby examples

---
+++ ex_inputs.rb
@@ -0,0 +1,7 @@
+print "What's your name? "
+name = gets() # get some input into variable name
+
+# Different ways to evaluate input
+puts("Hello, #{name}")
+puts("Hello, #{name.length}")
+puts("Hello, #{name.inspect}")
